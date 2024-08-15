e528167a232d1d46dff4b5d969a9115ce17d83eb
prince mumo <princeinkunzi@gmail.com>
2024-08-15 23:24:16 +0300

    upload fix

--- client/src/components/AdminRecipeCard.js
+++ client/src/components/AdminRecipeCard.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 const AdminRecipeCard = ({ recipe, onDelete }) => {
     return (
         <div className="admin-recipe-card">
-            {/* Fix the image source to match the database field */}
             <img src={recipe.main_photo} alt={recipe.name} style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
             
             <h3>{recipe.name}</h3>
